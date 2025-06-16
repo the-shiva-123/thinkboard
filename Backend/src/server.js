6426da01ed1e7f74100f94539ef09242243081c6
Avula Siva Sankar <sivasankaravula1.in>
2025-06-16 18:02:07 +0530

    prepared for the deplyment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from "cors";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import path from 'path';
 
 
 import notesRoutes from './Routes/notesRoutes.js';
@@ -11,15 +12,19 @@ dotenv.config();
 
 console.log(process.env.MONGO_URI); 
 const app=express();
-const PORT =process.env.PORT || 5001
+const PORT =process.env.PORT || 5001;
+const __dirname=path.resolve()
 
 
 //middleware    
-app.use(cors(
+if(process.env.NODE_ENV !=="production"){
+  app.use(cors(
     {
         origin : "http://localhost:5173",
     }
 ));
+}
+
 app.use(express.json());
 app.use(rateLimiter)
 
@@ -30,6 +35,15 @@ app.use(rateLimiter)
 // })
 
 app.use("/api/notes",notesRoutes);
+
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+    app.get("*",(res,req)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+    })
+}
 
 connectDB().then(()=>{
 app.listen(PORT,()=>{
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+const BASR_URL = import.meta.env.MODE === "development"? "http://localhost:5001/api":"/api"
 const api=axios.create({
-    baseURL : "http://localhost:5001/api"
+    baseURL : BASR_URL
 });
 
 
